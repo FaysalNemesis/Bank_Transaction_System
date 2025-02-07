a2fb4209756bb4b57dc523ccd3e6feeef6e3ab00
FaysalNemesis <faysalahammednstu13@gmail.com>
2025-02-08 00:11:10 +0600

    implement CURD

---
+++ Controllers/BankTransactionController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BankTransaction_App.Models;
+
+namespace BankTransaction_App.Controllers
+{
+    public class BankTransactionController : Controller
+    {
+        private readonly TransactionDbContext _context;
+
+        public BankTransactionController(TransactionDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: BankTransaction
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Transactions.ToListAsync());
+        }
+
+        // GET: BankTransaction/AddOrEdit
+        public IActionResult AddOrEdit(int id=0)
+        {
+            if (id == 0)
+                return View(new BankTransaction());
+            else
+                return View(_context.Transactions.Find(id));
+        }
+
+        // POST: BankTransaction/AddOrEdit
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit([Bind("TransactionId,AccountNumber,BenificiaryName,BankName,SWIFTCode,Amount,TransactionDate")] BankTransaction bankTransaction)
+        {
+            if (ModelState.IsValid)
+            {
+                if (bankTransaction.TransactionId ==0 )
+                {
+                    bankTransaction.TransactionDate = DateTime.Now;
+                    _context.Add(bankTransaction);
+                }
+                else
+                    _context.Update(bankTransaction);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bankTransaction);
+        }
+
+        // POST: BankTransaction/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var bankTransaction = await _context.Transactions.FindAsync(id);
+            if (bankTransaction != null)
+            {
+                _context.Transactions.Remove(bankTransaction);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+      
+    }
+}
--- Models/BankTransaction.cs
+++ Models/BankTransaction.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BankTransaction_App.Models
@@ -8,19 +9,30 @@ namespace BankTransaction_App.Models
         [Key]
         public int TransactionId { get; set; }
 
+        [Required(ErrorMessage = "Please enter account number")]
         [Column(TypeName = "varchar(12)")]
+        [DisplayName("Account Number")]
+        [MaxLength(12, ErrorMessage = "Maximum 12 characters only.")]
         public string AccountNumber { get; set; }
 
         [Column(TypeName = "varchar(100)")]
+        [Required(ErrorMessage = "Please enter benificiaery name")]
+        [DisplayName("Benificary Name")]
         public string BenificiaryName { get; set; }
 
         [Column(TypeName = "varchar(100)")]
+        [DisplayName("Bank Name")]
         public string BankName { get; set; }
 
+        [DisplayName("SWIFT Number")]
+        [Required(ErrorMessage = "Please enter SWIFT number")]
         [Column(TypeName = "varchar(11)")]
+        [MaxLength(11, ErrorMessage ="Maximum 11 characters only")]
         public string SWIFTCode { get; set; }
 
         public int? Amount { get; set; }
+        [DisplayName("Date")]
+        [DisplayFormat(DataFormatString = "{0: dd-MMM-yy}")]
         public DateTime TransactionDate { get; set; }
 
     }
